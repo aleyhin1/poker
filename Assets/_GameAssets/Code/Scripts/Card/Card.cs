b36d53eaf6aee74f1bc52350a69f4800e99402a1
Batu <aleyhin1@gmail.com>
2024-04-07 21:07:54 +0300

    card textures added

--- Assets/_GameAssets/Code/Scripts/Card/Card.cs
+++ Assets/_GameAssets/Code/Scripts/Card/Card.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    public Tuple<CardSuit, CardRank> CardValue { get; private set; }
+    public Tuple<CardSuit, CardRank> Value { get; private set; }
+    private Texture2D _sprite;
 
-    public void CreateCard(CardSuit suit, CardRank rank)
+    public void CreateCard(CardSuit suit, CardRank rank, Texture2D sprite)
     {
-        CardValue = new Tuple<CardSuit, CardRank>(suit, rank);
+        Value = new Tuple<CardSuit, CardRank>(suit, rank);
+        _sprite = sprite;
     }
 }
--- Assets/_GameAssets/Code/Scripts/Managers/DeckManager.cs
+++ Assets/_GameAssets/Code/Scripts/Managers/DeckManager.cs
@@ -7,10 +7,33 @@ public class DeckManager : MonoBehaviour
 {
     public static Stack<Card> Deck { get; private set; } = new Stack<Card>();
     [SerializeField] private Card _cardPrefab;
+    [Header("Card Textures")]
+    [Tooltip("Order in according to CardRank Enum")]
+    [SerializeField] private Texture2D[] _clubTexturesInOrder = new Texture2D[13];
+    [Tooltip("Order in according to CardRank Enum")]
+    [SerializeField] private Texture2D[] _diamondTexturesInOrder = new Texture2D[13];
+    [Tooltip("Order in according to CardRank Enum")]
+    [SerializeField] private Texture2D[] _heartTexturesInOrder = new Texture2D[13];
+    [Tooltip("Order in according to CardRank Enum")]
+    [SerializeField] private Texture2D[] _spadeTexturesInOrder = new Texture2D[13];
+    private Dictionary<CardSuit, Texture2D[]> _suitTexturesPairs;
+
 
     private void Awake()
     {
+        FillSuitTexturesPairs();
         FillDeck();
+    }
+
+    private void FillSuitTexturesPairs()
+    {
+        _suitTexturesPairs = new Dictionary<CardSuit, Texture2D[]>
+        {
+            { CardSuit.Club, _clubTexturesInOrder },
+            { CardSuit.Diamond, _diamondTexturesInOrder },
+            { CardSuit.Heart, _heartTexturesInOrder },
+            { CardSuit.Spade, _spadeTexturesInOrder },
+        };
     }
 
     private void FillDeck()
@@ -20,9 +43,13 @@ public class DeckManager : MonoBehaviour
             foreach (CardRank rank in Enum.GetValues(typeof(CardRank)))
             {
                 Card spawnedCard = CreateCardObject();
-                spawnedCard.CreateCard(suit, rank);
 
-                Debug.Log($"{spawnedCard.CardValue} spawned");
+                _suitTexturesPairs.TryGetValue(suit, out Texture2D[] cardTextures);
+                Texture2D cardTexture = cardTextures.GetValue((int)rank) as Texture2D;
+
+                spawnedCard.CreateCard(suit, rank, cardTexture);
+
+                Debug.Log($"{spawnedCard.Value} spawned with texture {cardTexture}");
             }
         }
     }
