7a56d7a35ed59bddbb95f7d78cf423079fb167bc
Batu <aleyhin1@gmail.com>
2024-04-28 17:48:48 +0300

    code cleanup

--- Assets/_GameAssets/_Scripts/Players/Player.cs
+++ Assets/_GameAssets/_Scripts/Players/Player.cs
@@ -3,39 +3,25 @@ using System.Collections.Generic;
 
 public abstract class Player : MonoBehaviour
 {
-    [SerializeField] private List<Card> _cards = new List<Card>(2);
+    public SpriteRenderer SpriteRenderer;
+    public int TotalMoney;
+    public bool IsSmallBlindPaid;
+    public bool IsBigBlindPaid;
+    public bool IsBigBlind;
+    public bool IsSmallBlind;
+    public bool IsFold;
+    [field : SerializeField] public List<Card> Cards { get; private set; } = new List<Card>(2);
     [SerializeField] private Transform _cardPos1, _cardPos2;
-    [SerializeField] private Color _defaultColor;
-    [SerializeField] private bool _isMyTurn;
-    [SerializeField] private bool _isSmallBlindPaid;
-    [SerializeField] private bool _isBigBlindPaid;
-    [SerializeField] private bool _isBigBlind;
-    [SerializeField] private bool _isSmallBlind;
-    private SpriteRenderer _spriteRenderer;
-    private bool _isFold;
-    private int _totalMoney;
+    [field : SerializeField] public Color DefaultColor { get; private set; }
+    public virtual bool IsMyTurn { get; set; }
 
-    public virtual List<Card> GetCards { get { return _cards; } }
-    public Color DefaultColor { get { return _defaultColor; }}
-    public SpriteRenderer SpriteRenderer { get { return _spriteRenderer; } set { _spriteRenderer = value; } }
-  
-    public int TotalMoney { get { return _totalMoney; } set { _totalMoney = value; } }
-
-    public bool IsFold { get { return _isFold; } set { _isFold = value; } }
-
-    public bool IsSmallBlind { get { return _isSmallBlind; } set { _isSmallBlind = value; } }
-    public bool IsSmallBlindPaid { get { return _isSmallBlindPaid; } set { _isSmallBlindPaid = value; } }
-    public bool IsBigBlind { get { return _isBigBlind; } set { _isBigBlind = value; } }
-    public bool IsBigBlindPaid { get { return _isBigBlindPaid; } set { _isBigBlindPaid = value; } }
-
-    public virtual bool IsMyTurn{ get { return _isMyTurn; } set {_isMyTurn = value; } }
 
     public (Vector3 pos, Quaternion rot) AddCards(Card card)
     {
         Quaternion rot = card.transform.rotation;
         Vector3 pos = card.transform.position;
         
-        if (_cards.Count < 1)
+        if (Cards.Count < 1)
         {
             pos = _cardPos1.position;
             rot = _cardPos1.rotation;
@@ -45,7 +31,7 @@ public abstract class Player : MonoBehaviour
             pos = _cardPos2.position;
             rot = _cardPos2.rotation;
         }
-        _cards.Add(card);
+        Cards.Add(card);
         card.gameObject.SetActive(true);
 
         return (pos, rot);
